3a12a5080f6c05dfb098e3cc66327fe9a68294d4
jkaho <72959522+jkaho@users.noreply.github.com>
2020-11-29 17:33:13 +1100

    Modify 'script.js' to add string variables for password character types

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVQXYZ"
+  var lowercase = "abcdefghijklmnopqrstuvqxyz"
+  var numbers = "0123456789"
+  var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
   var passwordLength = parseInt(prompt("How long do you want your password to be? Type in a numerical value between 8 to 128 (inclusive)."));
   console.log(passwordLength);
   if (isNaN(passwordLength) === true || passwordLength < 8 || passwordLength > 128) { 
