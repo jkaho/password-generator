0a2348e62f276e258e5813ef9a9f957ad3284f7c
jkaho <72959522+jkaho@users.noreply.github.com>
2020-11-29 18:16:23 +1100

    Modify 'script.js' to adjust line indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,73 +11,73 @@ function writePassword() {
   var chosenCharactersIndex = 0
   
   for (var i = 0; i < 1; i++) {
-      var passwordLength = parseInt(prompt("How long do you want your password to be? Type in a numerical value between 8 to 128 (inclusive)."));
-      console.log(passwordLength);
-      if (isNaN(passwordLength) === true || passwordLength < 8 || passwordLength > 128) { 
-          var wrongLengthInput = confirm("Must input a numerical value between 8 and 128 (inclusive).");
-          if (wrongLengthInput === true) {
-              i -= 1;
-          } else {
-            return;
-          }
+    var passwordLength = parseInt(prompt("How long do you want your password to be? Type in a numerical value between 8 to 128 (inclusive)."));
+    console.log(passwordLength);
+    if (isNaN(passwordLength) === true || passwordLength < 8 || passwordLength > 128) { 
+      var wrongLengthInput = confirm("Must input a numerical value between 8 and 128 (inclusive).");
+      if (wrongLengthInput === true) {
+        i -= 1;
+      } else {
+        return;
       }
+    }
   }
 
   for (var i = 0; i < 1; i++) {
-      var includeLowercase = confirm("Do you want to include lowercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
-      if (includeLowercase) {
-          chosenCharacters += lowercase;
-      }
-      if (includeLowercase) {
-          chosenCharactersIndex += 26;
-      }
-      console.log(chosenCharacters);
-      console.log(chosenCharactersIndex);
+    var includeLowercase = confirm("Do you want to include lowercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
+    if (includeLowercase) {
+      chosenCharacters += lowercase;
+    }
+    if (includeLowercase) {
+      chosenCharactersIndex += 26;
+    }
+    console.log(chosenCharacters);
+    console.log(chosenCharactersIndex);
 
-      var includeUppercase = confirm("Do you want to include uppercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
-      if (includeUppercase) {
-          chosenCharacters += uppercase;
-      }
-      if (includeUppercase) {
-          chosenCharactersIndex += 26;
-      }
-      console.log(chosenCharacters);
-      console.log(chosenCharactersIndex);
+    var includeUppercase = confirm("Do you want to include uppercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
+    if (includeUppercase) {
+      chosenCharacters += uppercase;
+    }
+    if (includeUppercase) {
+      chosenCharactersIndex += 26;
+    }
+    console.log(chosenCharacters);
+    console.log(chosenCharactersIndex);
 
-      var includeNumbers = confirm("Do you want to include numbers in your password? Press 'OK' for yes and 'Cancel' for no.");
-      if (includeNumbers) {
-          chosenCharacters += numbers;
-      }
-      if (includeNumbers) {
-          chosenCharactersIndex += 10;
-      }
-      console.log(chosenCharacters);
-      console.log(chosenCharactersIndex);
+    var includeNumbers = confirm("Do you want to include numbers in your password? Press 'OK' for yes and 'Cancel' for no.");
+    if (includeNumbers) {
+      chosenCharacters += numbers;
+    }
+    if (includeNumbers) {
+      chosenCharactersIndex += 10;
+    }
+    console.log(chosenCharacters);
+    console.log(chosenCharactersIndex);
 
-      var includeSpecialCharacters = confirm("Do you want to include special characters in your password? Press 'OK' for yes and 'Cancel' for no.");
-      if (includeSpecialCharacters) {
-          chosenCharacters += specialCharacters;
+    var includeSpecialCharacters = confirm("Do you want to include special characters in your password? Press 'OK' for yes and 'Cancel' for no.");
+    if (includeSpecialCharacters) {
+      chosenCharacters += specialCharacters;
+    }
+    if (includeSpecialCharacters) {
+      chosenCharactersIndex += 33;
+    }
+    if (includeLowercase === false && includeUppercase === false && includeNumbers === false && includeSpecialCharacters === false) {
+      var wrongCharacterInput= confirm("You must select at least one of the four character types."); 
+      if (wrongCharacterInput === true) {
+        i -= 1;
       }
-      if (includeSpecialCharacters) {
-          chosenCharactersIndex += 33;
-      }
-      if (includeLowercase === false && includeUppercase === false && includeNumbers === false && includeSpecialCharacters === false) {
-          var wrongCharacterInput= confirm("You must select at least one of the four character types."); 
-          if (wrongCharacterInput === true) {
-              i -= 1;
-          }
-      }
-      console.log(chosenCharacters);
-      console.log(chosenCharactersIndex);
+    }
+    console.log(chosenCharacters);
+    console.log(chosenCharactersIndex);
   }
 
   function generatePassword() {
-      var createPassword = "";
-      for (var i = 0; i < passwordLength; i++) {
-          createPassword += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharactersIndex));
-      }
-      console.log(createPassword);
-      return createPassword;
+    var createPassword = "";
+    for (var i = 0; i < passwordLength; i++) {
+      createPassword += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharactersIndex));
+    }
+    console.log(createPassword);
+    return createPassword;
   }
 
   var password = generatePassword();
@@ -85,8 +85,6 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
-writePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
