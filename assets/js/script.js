af857ba7b4c3b452381981be2f25fb86737edc2f
jkaho <72959522+jkaho@users.noreply.github.com>
2020-11-29 18:06:18 +1100

    Modify 'script.js' to add for loop to character type confirm methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,64 +23,67 @@ function writePassword() {
       }
   }
 
-  var includeLowercase = confirm("Do you want to include lowercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
-  if (includeLowercase) {
-      chosenCharacters += lowercase;
-  }
-  if (includeLowercase) {
-      chosenCharactersIndex += 26;
-  }
-  console.log(chosenCharacters);
-  console.log(chosenCharactersIndex);
+  for (var i = 0; i < 1; i++) {
+      var includeLowercase = confirm("Do you want to include lowercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
+      if (includeLowercase) {
+          chosenCharacters += lowercase;
+      }
+      if (includeLowercase) {
+          chosenCharactersIndex += 26;
+      }
+      console.log(chosenCharacters);
+      console.log(chosenCharactersIndex);
 
-  var includeUppercase = confirm("Do you want to include uppercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
-  if (includeUppercase) {
-      chosenCharacters += uppercase;
-  }
-  if (includeUppercase) {
-      chosenCharactersIndex += 26;
-  }
-  console.log(chosenCharacters);
-  console.log(chosenCharactersIndex);
+      var includeUppercase = confirm("Do you want to include uppercase characters in your password? Press 'OK' for yes and 'Cancel' for no.");
+      if (includeUppercase) {
+          chosenCharacters += uppercase;
+      }
+      if (includeUppercase) {
+          chosenCharactersIndex += 26;
+      }
+      console.log(chosenCharacters);
+      console.log(chosenCharactersIndex);
 
-  var includeNumbers = confirm("Do you want to include numbers in your password? Press 'OK' for yes and 'Cancel' for no.");
-  if (includeNumbers) {
-      chosenCharacters += numbers;
-  }
-  if (includeNumbers) {
-      chosenCharactersIndex += 10;
-  }
-  console.log(chosenCharacters);
-  console.log(chosenCharactersIndex);
+      var includeNumbers = confirm("Do you want to include numbers in your password? Press 'OK' for yes and 'Cancel' for no.");
+      if (includeNumbers) {
+          chosenCharacters += numbers;
+      }
+      if (includeNumbers) {
+          chosenCharactersIndex += 10;
+      }
+      console.log(chosenCharacters);
+      console.log(chosenCharactersIndex);
 
-  var includeSpecialCharacters = confirm("Do you want to include special characters in your password? Press 'OK' for yes and 'Cancel' for no.");
-            
-  if (includeSpecialCharacters) {
-      chosenCharacters += specialCharacters;
+      var includeSpecialCharacters = confirm("Do you want to include special characters in your password? Press 'OK' for yes and 'Cancel' for no.");
+      if (includeSpecialCharacters) {
+          chosenCharacters += specialCharacters;
+      }
+      if (includeSpecialCharacters) {
+          chosenCharactersIndex += 33;
+      }
+      if (includeLowercase === false && includeUppercase === false && includeNumbers === false && includeSpecialCharacters === false) {
+          var wrongCharacterInput= confirm("You must select at least one of the four character types."); 
+          if (wrongCharacterInput === true) {
+              i -= 1;
+          }
+      }
+      console.log(chosenCharacters);
+      console.log(chosenCharactersIndex);
   }
-  if (includeSpecialCharacters) {
-      chosenCharactersIndex += 33;
-  }
-  if (includeLowercase === false && includeUppercase === false && includeNumbers === false && includeSpecialCharacters === false) {
-    var wrongCharacterInput= confirm("You must select at least one of the four character types."); 
-  }
-  console.log(chosenCharacters);
-  console.log(chosenCharactersIndex);
 
   function generatePassword() {
-    var createPassword = "";
-    for (var i = 0; i < passwordLength; i++) {
-        createPassword += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharactersIndex));
-    }
-    console.log(createPassword);
-    return createPassword;
-    }
+      var createPassword = "";
+      for (var i = 0; i < passwordLength; i++) {
+          createPassword += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharactersIndex));
+      }
+      console.log(createPassword);
+      return createPassword;
+  }
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 writePassword();
